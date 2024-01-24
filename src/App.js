3eec7a942d4a222f5774508f7cc3ecf7bef9a4c9
Bubolie <elodie41.m@gmail.com>
2024-01-24 20:46:56 +0300

    Création initiale de la structure du site avec de différents composants, pages, fichier main.scss, importation image et police, base de données

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./main.scss";
+
+import Home from "./pages/Home/Home";
+import NotFound from "./pages/NotFound/NotFound";
+import Legal from "./pages/Legal/Legal";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bonjour</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/page404" element={<NotFound />} />
+        <Route path="/legal" element={<Legal />} />
+        <Route path="/" element={<Home />} />
+
+      </Routes>
     </div>
   );
 }
