c03d328398077096bd9bf3eacb2980207adef7c6
Bubolie <elodie41.m@gmail.com>
2024-01-31 23:21:14 +0300

    création et mise en page de la page 404 et du composant Button

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Button from "../../components/Button/Button";
+import img404 from "../../assets/images/error-404.png";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
+    <div className="container body-wrapper my-5">
+      <h1>Erreur 404</h1>
+      <div className="container-fluid text-center">
+        <img src={img404} alt='logo ordinateur affichant "404"' />
+      </div>
+      <p>Page non trouvée...</p>
+      <Button
+        linkPage="/"
+        type="button"
+        buttonName="Retourner à la page d'accueil"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
