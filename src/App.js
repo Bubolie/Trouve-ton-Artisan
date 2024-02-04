4a64565fd028d13b9508d80401581e2b2204a693
Bubolie <elodie41.m@gmail.com>
2024-02-04 23:18:11 +0300

    Création du composant Card, mise en place du fichier color.scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import "./main.scss";
+import "../src/assets/style.scss/main.scss";
 
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,35 @@
-const Card = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import React from "react";
+import "./card.scss";
+import Button from "../Button/Button";
+import star from "../../assets/images/star-32.png";
+import location from "../../assets/images/location-24.png"
+import category from "../../assets/images/categories-24.png"
 
-export default Card
+const Card = (props) => {
+  return (
+    <div className="card shadow m-3">
+      <div className="m-4 card-body shadow">
+        <h3 className="card-title text-center ">{props.nameEntreprise}</h3>
+        <div className="card-text">
+          <div className="d-flex align-items-center justify-content-center">
+            <img src={star} alt="Etoile jaune"  />
+            <p className="m-3">{props.note}/5</p>
+          </div>
+          <div className="d-flex align-items-center">
+            <img src={category} alt="" className="logo img-thumbnail"/>
+            <p className="m-2">{props.category}</p></div>
+          <div className="d-flex align-items-center">
+            <img src={location} alt="" className="logo img-thumbnail"/>
+          <p className="m-2">{props.location}</p></div>
+          
+        </div>
+      </div>
+      <div className="text-center mb-3">
+              <Button linkPage="#" type="link" onClick="#" buttonName="En savoir plus" />
+
+      </div>
+    </div>
+  );
+};
+
+export default Card;
