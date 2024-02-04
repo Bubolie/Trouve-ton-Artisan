135bdb5bfadaeacdc8f0af4a23b11424a91f8a71
Bubolie <elodie41.m@gmail.com>
2024-02-04 23:18:11 +0300

    Création du composant Card, mise en place du fichier color.scss

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Card from "../../components/Card/Card";
 
 const Home = () => {
   return (
     <main className="container">
       <h1>Accueil</h1>
+      <Card nameEntreprise="Didier" note="2.6" category="Fabrication" location="Lyon"/>
     </main>
 
   )
