a1a75cce698988ada0fa054a27d6c38c0f277ad4
Bubolie <elodie41.m@gmail.com>
2024-02-29 23:01:10 +0300

    correction des erreurs de vulnérabilité et mise à jour

--- src/pages/EntrepriseDetails/FormContactArtisan.jsx
+++ src/pages/EntrepriseDetails/FormContactArtisan.jsx
@@ -1,36 +1,38 @@
 import Button from "../../components/Button/Button";
 
 const FormContactArtisan = (props) => {
-
   const handleSubmit = (e) => {
-    alert("Votre message a bien été envoyé à l'artisan " + props.name + ".")
+    alert("Votre message a bien été envoyé à l'artisan " + props.name + ".");
     e.preventDefault();
     const form = e.target;
     form.reset();
   };
 
   return (
-    <form action="" method="post" 
-    onSubmit={handleSubmit}
-    >
+    <form action="" method="post" onSubmit={handleSubmit}>
       <div className="row m-3">
-        <input
-          className="px-2 my-2 rounded-4 form-text-color col-lg-6"
-          type="text"
-          name="name"
-          placeholder="Nom"
-          autoComplete="name"
-          required
-        />
-        <input
-          className="px-2 my-2 rounded-4 form-text-color col-lg-6"
-          type="text"
-          name="objet"
-          placeholder="Objet"
-          required
-        />
+        <div className="col-lg-6 p-0 pe-lg-3">
+          <input
+            className="container rounded-4 form-text-color my-2 "
+            type="text"
+            name="name"
+            placeholder="Nom"
+            autoComplete="name"
+            required
+          />
+        </div>
+        <div className="col-lg-6 p-0 ps-lg-3">
+          <input
+            className="container rounded-4 form-text-color my-2"
+            type="text"
+            name="objet"
+            placeholder="Objet"
+            required
+          />
+        </div>
+
         <textarea
-          className="px-2 my-2 rounded-4 form-text-color"
+          className="container my-2 rounded-4 form-text-color"
           name="message"
           id="message"
           rows="10"
